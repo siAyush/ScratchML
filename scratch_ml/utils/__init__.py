c57c8c8dc563ffc47cef4c42760a3244987eaa39
siAyush <atulg7516@gmail.com>
2020-12-29 23:39:05 +0530

    activation functions

--- scratch_ml/utils/__init__.py
+++ scratch_ml/utils/__init__.py
@@ -1,2 +1,3 @@
 from .data_operation import *
 from .plot import Plot
+from .activation_functions import *
---
+++ scratch_ml/utils/activation_functions.py
@@ -0,0 +1,10 @@
+# activation functions
+import numpy as np
+
+class Sigmoid():
+    def __call__(self, x):
+        return 1/(1+np.exp(-x))
+    
+    def derivative(self, x):
+        f = self.__call__(x)
+        return f * (1-f)
