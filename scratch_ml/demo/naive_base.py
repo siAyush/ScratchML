dbf6f52fbc8e1e764ce54c589f1141370de336db
siAyush <atulg7516@gmail.com>
2021-01-06 22:35:30 +0530

    naive base

---
+++ scratch_ml/demo/naive_base.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn import datasets
+from scratch_ml.utils import train_test_split, normalize, accuracy_score, Plot
+from scratch_ml.supervised_learning import NaiveBayes
+
+
+def main():
+    data = datasets.load_digits()
+    x = normalize(data.data)
+    y = data.target
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+    clf = NaiveBayes()
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print ("Accuracy:", accuracy)
+    Plot().plot_2d(x_test, y_pred, title="Naive Bayes", accuracy=accuracy, legend_label=data.target_names)
+
+
+if __name__ == "__main__":
+    main()
