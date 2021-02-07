5318a185d3af37cfd26daf0f93922537bc87155c
siayush <atulg7516@gmail.com>
2021-02-08 00:59:30 +0530

    K means demo added

--- scratch_ml/demo/k_means.py
+++ scratch_ml/demo/k_means.py
@@ -5,8 +5,14 @@ from scratch_ml.unsupervised_learning import KMeans
 
 
 def main():
-    pass
+    x, y = datasets.make_blobs()
 
+    clf = KMeans(k=3)
+    y_pred = clf.predict(x)
+
+    p = Plot()
+    p.plot_2d(x, y_pred, title="K-Means Clustering")
+    p.plot_2d(x, y, title="Actual Clustering")
 
 
 if __name__ == "__main__":
--- scratch_ml/unsupervised_learning/k_means.py
+++ scratch_ml/unsupervised_learning/k_means.py
@@ -9,7 +9,7 @@ class KMeans():
     def __init__(self, k=2, iterations=500):
         self.k = k
         self.iterations = iterations
-    
+        
 
     def _init_random_centroids(self, x):
         """Initialize the centroids"""
@@ -19,47 +19,47 @@ class KMeans():
             centroid = x[np.random.choice(range(n_samples))]
             centroids[i] = centroid
         return centroids
-    
+
 
     def _closest_centroid(self, sample, centroids):
         """Return the index of the closest centroid to the sample"""
         closest_idx = 0
-        closest_dist = float("inf")
+        closest_dist = float('inf')
         for i, centroid in enumerate(centroids):
             distance = euclidean_distance(sample, centroid)
             if distance < closest_dist:
-                closest_dist = distance
                 closest_idx = i
+                closest_dist = distance
         return closest_idx
-    
 
-    def _create_clusters(self, x, centroids):
+
+    def _create_clusters(self, centroids, x):
         """Assign the samples to the closest centroids to create clusters"""
-        clusters = [[] for i in range(self.k)]
+        clusters = [[] for _ in range(self.k)]
         for sample_idx, sample in enumerate(x):
             centroid_idx = self._closest_centroid(sample, centroids)
             clusters[centroid_idx].append(sample_idx)
         return clusters
-    
 
-    def _calculate_centroids(self, x, clusters):
-        """Calculate new centroids as the means of the samples in each cluster"""
+
+    def _calculate_centroids(self, clusters, x):
+        """Calculate new centroids"""
         n_features = np.shape(x)[1]
         centroids = np.zeros((self.k, n_features))
-        for idx, cluster in enumerate(clusters):
+        for i, cluster in enumerate(clusters):
             centroid = np.mean(x[cluster], axis=0)
-            centroids[idx] = centroid
+            centroids[i] = centroid
         return centroids
-    
 
-    def _get_cluster_labels(self, x, clusters):
+
+    def _get_cluster_labels(self, clusters, x):
         """Classify samples as the index of their clusters"""
         y_pred = np.zeros(np.shape(x)[0])
-        for cluster_idx, cluster in enumerate(clusters):
-            for sample_idx in cluster:
-                y_pred[sample_idx] = cluster_idx
-        return y_pred   
-    
+        for cluster_i, cluster in enumerate(clusters):
+            for sample_i in cluster:
+                y_pred[sample_i] = cluster_i
+        return y_pred
+
 
     def predict(self, x):
         centroids = self._init_random_centroids(x)
