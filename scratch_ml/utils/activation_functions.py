06a5af138b2e22c8f36f1d3d15dfd57131dba5a0
siayush <atulg7516@gmail.com>
2021-03-01 23:24:06 +0530

    activation function updated

--- scratch_ml/utils/activation_functions.py
+++ scratch_ml/utils/activation_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.lib.function_base import select
 
 
 class Sigmoid():
@@ -19,3 +20,30 @@ class Softmax():
     def gradient(self, x):
         p = self.__call__(x)
         return p * (1 - p)
+
+
+class RELU():
+    def __call__(self, x):
+        return np.where(x >= 0, x, 0)
+
+    def gradient(self, x):
+        return np.where(x >= 0, 1, 0)
+
+
+class LeakyReLU():
+    def __init__(self, alpha=0.1):
+        self.alpha = alpha
+
+    def __call__(self, x):
+        return np.where(x >= 0, x, self.alpha * x)
+
+    def gradient(self, x):
+        return np.where(x >= 0, 1, self.alpha)
+
+
+class TanH():
+    def __call__(self, x):
+        return 2 / (1 + np.exp(-2*x)) - 1
+
+    def gradient(self, x):
+        return 1 - np.power(self.__call__(x), 2)
