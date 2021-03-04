6b46b6d538255a7c9da68ae9b9c1ed7702b25d9e
siayush <atulg7516@gmail.com>
2021-03-04 23:49:15 +0530

    RMSprop added

--- scratch_ml/deep_learning/optimizers.py
+++ scratch_ml/deep_learning/optimizers.py
@@ -12,6 +12,30 @@ class StochasticGradientDescent():
         # If not initialized
         if self.weight_updt is None:
             self.weight_updt = np.zeros(np.shape(weight))
+        # Use momentum if set
         self.weight_updt = self.momentum * \
             self.weight_updt + (1 - self.momentum) * grad_wrt_w
         return weight - self.learning_rate * self.weight_updt
+
+
+class Adagrad():
+    pass
+
+
+class RMSprop():
+    def __init__(self, learning_rate=0.01, rho=0.9):
+        self.learning_rate = learning_rate
+        self.Eg = None  # Running average of the square gradients at w
+        self.eps = 1e-8
+        self.rho = rho
+
+    def update(self, w, grad_wrt_w):
+        # If not initialized
+        if self.Eg is None:
+            self.Eg = np.zeros(np.shape(grad_wrt_w))
+        self.Eg = self.rho * self.Eg + (1 - self.rho) * np.power(grad_wrt_w, 2)
+        return w - self.learning_rate * grad_wrt_w / np.sqrt(self.Eg + self.eps)
+
+
+class Adam():
+    pass
