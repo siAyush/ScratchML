0122fedfe3025116133b1366b339e3a33f5c0afa
siayush <atulg7516@gmail.com>
2021-03-05 21:51:29 +0530

    Adam, Adagrad

--- scratch_ml/deep_learning/optimizers.py
+++ scratch_ml/deep_learning/optimizers.py
@@ -19,10 +19,23 @@ class StochasticGradientDescent():
 
 
 class Adagrad():
-    pass
+    
+    def __init__(self, learning_rate=0.01):
+        self.learning_rate = learning_rate
+        self.G = None  # Sum of squares of the gradients
+        self.eps = 1e-8
+
+    def update(self, w, grad_wrt_w):
+        # If not initialized
+        if self.G is None:
+            self.G = np.zeros(np.shape(w))
+        # Add the square of the gradient of the loss function at w
+        self.G += np.power(grad_wrt_w, 2)
+        return w - self.learning_rate * grad_wrt_w / np.sqrt(self.G + self.eps)
 
 
 class RMSprop():
+
     def __init__(self, learning_rate=0.01, rho=0.9):
         self.learning_rate = learning_rate
         self.Eg = None  # Running average of the square gradients at w
@@ -38,4 +51,22 @@ class RMSprop():
 
 
 class Adam():
-    pass
+
+    def __init__(self, learning_rate=0.001, b1=0.9, b2=0.999):
+        self.learning_rate = learning_rate
+        self.eps = 1e-8
+        self.m = None
+        self.v = None
+        self.b1 = b1
+        self.b2 = b2
+
+    def update(self, w, grad_wrt_w):
+        if self.m is None:
+            self.m = np.zeros(np.shape(grad_wrt_w))
+            self.v = np.zeros(np.shape(grad_wrt_w))
+        self.m = self.b1 * self.m + (1 - self.b1) * grad_wrt_w
+        self.v = self.b2 * self.v + (1 - self.b2) * np.power(grad_wrt_w, 2)
+        m_hat = self.m / (1 - self.b1)
+        v_hat = self.v / (1 - self.b2)
+        self.w_updt = self.learning_rate * m_hat / (np.sqrt(v_hat) + self.eps)
+        return w - self.w_updt
