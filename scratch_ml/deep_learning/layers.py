fc2fa9c26247666a52b52d112244518c5c8480c5
siayush <atulg7516@gmail.com>
2021-03-13 16:33:36 +0530

    pooling layer added

--- scratch_ml/deep_learning/layers.py
+++ scratch_ml/deep_learning/layers.py
@@ -184,4 +184,138 @@ class Reshape(Layer):
 
 class PoolingLayer(Layer):
     """A parent class of MaxPooling and AveragePooling."""
-    pass
+
+    def __init__(self, pool_shape=(2, 2), stride=1, padding=0):
+        self.pool_shape = pool_shape
+        self.stride = stride
+        self.padding = padding
+        self.trainable = True
+
+    def forward_pass(self, x, training=True):
+        self.layer_input = x
+        batch_size, channels, height, width = x.shape
+        _, out_height, out_width = self.output_shape()
+        x = x.reshape(batch_size*channels, 1, height, width)
+        x_col = image_to_column(x, self.pool_shape, self.stride, self.padding)
+        # MaxPool or AveragePool method
+        output = self._pool_forward(x_col)
+        output = output.reshape(out_height, out_width, batch_size, channels)
+        output = output.transpose(2, 3, 0, 1)
+        return output
+
+    def backward_pass(self, gradient):
+        batch_size, _, _, _ = gradient.shape
+        channels, height, width = self.input_shape
+        accum_grad = gradient.transpose(2, 3, 0, 1).ravel()
+        # MaxPool or AveragePool method
+        accum_grad_col = self._pool_backward(accum_grad)
+        accum_grad = column_to_image(
+            accum_grad_col, (batch_size * channels, 1, height, width), self.pool_shape, self.stride, 0)
+        accum_grad = accum_grad.reshape((batch_size,) + self.input_shape)
+        return accum_grad
+
+    def output_shape(self):
+        channels, height, width = self.input_shape
+        out_height = (height - self.pool_shape[0]) / self.stride + 1
+        out_width = (width - self.pool_shape[1]) / self.stride + 1
+        assert out_height % 1 == 0
+        assert out_width % 1 == 0
+        return channels, int(out_height), int(out_width)
+
+
+class MaxPooling2D(PoolingLayer):
+    def _pool_forward(self, x_col):
+        arg_max = np.argmax(x_col, axis=0).flatten()
+        output = x_col[arg_max, range(arg_max.size)]
+        self.cache = arg_max
+        return output
+
+    def _pool_backward(self, gradient):
+        gradient_col = np.zeros((np.prod(self.pool_shape), gradient.size))
+        arg_max = self.cache
+        gradient_col[arg_max, range(gradient.size)] = gradient
+        return gradient_col
+
+
+class AveragePooling2D(PoolingLayer):
+    def _pool_forward(self, x_col):
+        output = np.mean(x_col, axis=0)
+        return output
+
+    def _pool_backward(self, gradient):
+        gradient_col = np.zeros((np.prod(self.pool_shape), gradient.size))
+        gradient_col[:, range(gradient.size)] = 1. / \
+            gradient_col.shape[0] * gradient
+        return gradient_col
+
+
+def determine_padding(filter_shape, output_shape="same"):
+    """Method which calculates the padding based on the specified output shape
+       and the shape of the filters."""
+    if output_shape == "valid":
+        return (0, 0)(0, 0)
+    elif output_shape == "same":
+        filter_height, filter_width = filter_shape
+        # output_height = (height + pad_h - filter_height) / stride + 1
+        pad_h1 = int(math.floor((filter_height - 1)/2))
+        pad_h2 = int(math.ceil((filter_height - 1)/2))
+        pad_w1 = int(math.floor((filter_width - 1)/2))
+        pad_w2 = int(math.ceil((filter_width - 1)/2))
+        return (pad_h1, pad_h2), (pad_w1, pad_w2)
+
+
+def get_im2col_indices(images_shape, filter_shape, padding, stride=1):
+    # First figure out what the size of the output should be
+    batch_size, channels, height, width = images_shape
+    filter_height, filter_width = filter_shape
+    pad_h, pad_w = padding
+    out_height = int((height + np.sum(pad_h) - filter_height) / stride + 1)
+    out_width = int((width + np.sum(pad_w) - filter_width) / stride + 1)
+    i0 = np.repeat(np.arange(filter_height), filter_width)
+    i0 = np.tile(i0, channels)
+    i1 = stride * np.repeat(np.arange(out_height), out_width)
+    j0 = np.tile(np.arange(filter_width), filter_height * channels)
+    j1 = stride * np.tile(np.arange(out_width), out_height)
+    i = i0.reshape(-1, 1) + i1.reshape(1, -1)
+    j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+    k = np.repeat(np.arange(channels), filter_height *
+                  filter_width).reshape(-1, 1)
+    return (k, i, j)
+
+
+def image_to_column(images, filter_shape, stride, output_shape="same"):
+    filter_height, filter_width = filter_shape
+    pad_h, pad_w = determine_padding(filter_shape, output_shape)
+    # Add padding to the image
+    images_padded = np.pad(
+        images, ((0, 0), (0, 0), pad_h, pad_w), mode='constant')
+    # Calculate the indices where the dot products are to be applied between weights
+    # and the image
+    k, i, j = get_im2col_indices(
+        images.shape, filter_shape, (pad_h, pad_w), stride)
+    # Get content from image at those indices
+    cols = images_padded[:, k, i, j]
+    channels = images.shape[1]
+    # Reshape content into column shape
+    cols = cols.transpose(1, 2, 0).reshape(
+        filter_height * filter_width * channels, -1)
+    return cols
+
+
+def column_to_image(cols, images_shape, filter_shape, stride, output_shape='same'):
+    batch_size, channels, height, width = images_shape
+    pad_h, pad_w = determine_padding(filter_shape, output_shape)
+    height_padded = height + np.sum(pad_h)
+    width_padded = width + np.sum(pad_w)
+    images_padded = np.zeros(
+        (batch_size, channels, height_padded, width_padded))
+    # Calculate the indices where the dot products are applied between weights
+    # and the image
+    k, i, j = get_im2col_indices(
+        images_shape, filter_shape, (pad_h, pad_w), stride)
+    cols = cols.reshape(channels * np.prod(filter_shape), -1, batch_size)
+    cols = cols.transpose(2, 0, 1)
+    # Add column content to the images at the indices
+    np.add.at(images_padded, (slice(None), k, i, j), cols)
+    # Return image without padding
+    return images_padded[:, :, pad_h[0]:height+pad_h[0], pad_w[0]:width+pad_w[0]]
